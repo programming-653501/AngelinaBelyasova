f20d6a9dc880f351ffd6e1c06ccf0198d658debf
Angelina Belyasova <belyasova98@mail.ru>
2017-05-16 18:21:20 +0300

    Course work
    telegram bot
    testing knowledge in English

---
+++ telegram/texts.py
@@ -0,0 +1,14 @@
+help_message = ["Hi, I'm a bot to help you figure out "
+                "where you currently are at a scale with your knowledge of English.\n"
+                "To manage me, please, consider using the following commands:\n"
+                "/help - to get this message once more in case you need reference\n"
+                "/test - to get testing started\n"
+                "/previous - to get your previous result\n"
+                "/stop - cuts testing down\n"
+                "You can find them with the help of a specified button near the input field."]
+
+before_start_message = "Before you start specify the level of English you want to prove."
+
+result = "You've done it.\nYour result is _{} correct answers_.\nIt is *{} %*. {}"
+
+correct_answers = "You can check out correct answers if you want. It's not advisable though."
