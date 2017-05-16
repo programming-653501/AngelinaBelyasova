3a313a9701d34f825a3e9e9b86cb42892a52ee17
Angelina Belyasova <belyasova98@mail.ru>
2017-05-16 22:38:25 +0300

    Update config.py

--- telegram/config.py
+++ telegram/config.py
@@ -1,4 +1,4 @@
-token = "341800604:AAEBicLNfzxyHsipB6wirkDkGMllnpDtdaE"
+token = "token"
 answers = "{level}{number}:answers"
 correct_answer = "{level}{number}:correct"
 question = "{level}{number}"
